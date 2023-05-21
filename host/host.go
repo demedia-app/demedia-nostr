075053c1ff00e9ef02624c517b7700f112107c9a
Sithum Bopitiya <sithumsandeepap@gmail.com>
2023-05-21 19:21:36 +0530

    Append msg to log

--- host/host.go
+++ host/host.go
@@ -15,10 +15,13 @@ import (
 func GetAdd(port string, isLocal string) string {
 	consensus := externalip.DefaultConsensus(nil, nil)
 	parsedIP, err := consensus.ExternalIP()
+	if err != nil {
+		log.Panic(fmt.Errorf("get external ip error: %v", err))
+	}
 	if isLocal == "1" {
 		addrs, err := net.InterfaceAddrs()
 		if err != nil {
-			log.Panic(err)
+			log.Panic(fmt.Errorf("get local ip error: %v", err))
 		}
 		for _, address := range addrs {
 			// check the address type and if it is not a loopback the display it
