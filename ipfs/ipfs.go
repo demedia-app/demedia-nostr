e2c48cf4d1aeb888a4a820102861957eecbd151c
Sithum Bopitiya <sithumsandeepap@gmail.com>
2023-06-22 10:35:11 +0530

    Go IPFS init

---
+++ ipfs/ipfs.go
@@ -0,0 +1,47 @@
+package ipfs
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	shell "github.com/ipfs/go-ipfs-api"
+)
+
+type IPFSClient struct {
+	sh *shell.Shell
+}
+
+func NewIPFSClient(apiAddress string) *IPFSClient {
+	sh := shell.NewShell(apiAddress)
+	return &IPFSClient{sh}
+}
+
+func (c *IPFSClient) UploadFile(data []byte) (string, error) {
+	cid, err := c.sh.Add(bytes.NewReader(data))
+	if err != nil {
+		return "", err
+	}
+	return cid, nil
+}
+
+func (c *IPFSClient) DownloadFile(cid string) ([]byte, error) {
+	reader, err := c.sh.Cat(cid)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (c *IPFSClient) DeleteFile(cid string) error {
+	err := c.sh.Unpin(cid)
+	if err != nil {
+		return err
+	}
+	return nil
+}
